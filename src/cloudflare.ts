438605acb95bf7bf370aae86bed4a6de23528b24
Jiri Spac <capajj@gmail.com>
2024-11-24 16:07:31 +0100

    initial commit

---
+++ src/cloudflare.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env bun
+
+import { readFileSync, readdirSync } from "fs";
+import { $ } from "bun";
+import { select } from "@inquirer/prompts";
+
+const getDotfiles = () => {
+  return readdirSync(process.cwd()).filter(
+    (file) => file.startsWith(".") && file !== ".git"
+  );
+};
+
+const cloudflare = async () => {
+  let envFile = process.argv[2];
+
+  if (!envFile) {
+    const dotfiles = getDotfiles();
+    const hasDotDevVars = dotfiles.includes(".dev.vars");
+    const answers = await select({
+      message: "Select a dotfile to use:",
+      choices: dotfiles.map((file) => ({ name: file, value: file })),
+      default: hasDotDevVars ? ".dev.vars" : dotfiles[0],
+    });
+    envFile = answers;
+  }
+
+  const createCloudflareSecret = async (key: string, value: string) => {
+    await $`echo ${value} | wrangler secret put ${key}`;
+  };
+
+  const envContent = readFileSync(envFile, "utf-8");
+  const envVariables = envContent
+    .split("\n")
+    .filter((line) => line.trim() && !line.startsWith("#"));
+
+  for (const envVar of envVariables) {
+    const [key, ...valueParts] = envVar.split("=");
+    const value = valueParts.join("=").trim();
+    await createCloudflareSecret(key.trim(), value);
+  }
+
+  console.log(`${envVariables.length} Cloudflare secrets set successfully`);
+};
+
+cloudflare();
---
+++ src/github.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env bun
+
+import { readFileSync, readdirSync } from "fs";
+import { $ } from "bun";
+import { select } from "@inquirer/prompts";
+
+const getDotfiles = () => {
+  return readdirSync(process.cwd()).filter(
+    (file) => file.startsWith(".") && file !== ".git"
+  );
+};
+
+const github = async () => {
+  let envFile = process.argv[2];
+
+  if (!envFile) {
+    const dotfiles = getDotfiles();
+    const hasDotEnv = dotfiles.includes(".env");
+    const hasDotDevVars = dotfiles.includes(".dev.vars");
+    const answers = await select({
+      message: "Select a dotfile to use:",
+      choices: dotfiles.map((file) => ({ name: file, value: file })),
+      default: hasDotEnv ? ".env" : hasDotDevVars ? ".dev.vars" : dotfiles[0],
+    });
+    envFile = answers;
+  }
+
+  // read current git repository owner and name from git remote
+  const gitRemoteOutput = await $`git remote get-url origin`;
+  const [owner, repo] = gitRemoteOutput.stdout
+    .toString()
+    .split(":")[1]
+    .replace(".git", "")
+    .split("/");
+
+  console.log(
+    `setting secrets from ${envFile} to Owner: ${owner}, Repo: ${repo}`
+  );
+
+  const createGitHubSecret = async (key: string, value: string) => {
+    await $`gh secret set ${key} -b "${value}"`;
+  };
+
+  const envContent = readFileSync(envFile, "utf-8");
+  const envVariables = envContent
+    .split("\n")
+    .filter((line) => line.trim() && !line.startsWith("#"));
+
+  await Promise.all(
+    envVariables.map(async (envVar) => {
+      const [key, ...valueParts] = envVar.split("=");
+      const value = valueParts.join("=").trim();
+      await createGitHubSecret(key.trim(), value);
+    })
+  );
+
+  console.log(`${envVariables.length} gitHub secrets set successfully`);
+};
+
+github();
